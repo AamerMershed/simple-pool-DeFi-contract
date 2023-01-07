2910a51c2086f3894174f3395737bda7477b6586
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-07 18:53:14 +0100

    Update def.js

--- PoolDefi/def.js
+++ PoolDefi/def.js
@@ -6,11 +6,11 @@ async function deposit(poolContract, amount) {
     // Deposit the specified amount into the contract
     await poolContract.methods.deposit().send({
         from: account,
-        value: web3.utils.toWei(amount, "ether")
+        value: web3.utils.toWei(amount, "ether")!
     });
 }
 
 async function withdraw(poolContract) {
     // Get the current account
     const accounts = await web3.eth.getAccounts();
-    const account = accounts!
+    const account = accounts
